eb39af99450b7c456dd4bfb53f9b997dda846022
doter <doter@example.com>
2015-07-09 18:08:35 +0800

    test

---
+++ WYD-WorldServer/src/com/wyd/empire/world/dao/mysql/IChatRecordDao.java
@@ -0,0 +1,9 @@
+package com.wyd.empire.world.dao.mysql;
+
+import com.wyd.db.mysql.dao.UniversalDao;
+
+/**
+ * The DAO interface for the TabChatRecord entity.
+ */
+public interface IChatRecordDao extends UniversalDao {
+}
