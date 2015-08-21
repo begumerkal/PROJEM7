04bcb64e03d7592cc0010740a25f93ccc7ff7a94
doter <doter@example.com>
2015-08-21 22:29:41 +0800

    test

---
+++ APP-DispatchServer/src/com/app/dispatch/vo/Map.java
@@ -0,0 +1,34 @@
+package com.app.dispatch.vo;
+
+public class Map {
+	private int id;
+	private int high;
+	private int width;
+	private int maxPlayer;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getHigh() {
+		return high;
+	}
+	public void setHigh(int high) {
+		this.high = high;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getMaxPlayer() {
+		return maxPlayer;
+	}
+	public void setMaxPlayer(int maxPlayer) {
+		this.maxPlayer = maxPlayer;
+	}
+
+}
---
+++ APP-DispatchServer/src/com/app/dispatch/vo/Player.java
@@ -0,0 +1,27 @@
+package com.app.dispatch.vo;
+
+public class Player {
+	private String nickname;
+	private int heroId;
+	private int lv;
+
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getHeroId() {
+		return heroId;
+	}
+	public void setHeroId(int heroId) {
+		this.heroId = heroId;
+	}
+	public int getLv() {
+		return lv;
+	}
+	public void setLv(int lv) {
+		this.lv = lv;
+	}
+
+}
