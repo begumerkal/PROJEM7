8b476ed42ef4cae848e2929a9705a1d6b183f4c5
doter <doter@example.com>
2015-09-01 14:46:58 +0800

    test

---
+++ APP-DispatchServer/src/com/app/dispatch/vo/ClientInfo.java
@@ -0,0 +1,95 @@
+package com.app.dispatch.vo;
+
+import java.util.Timer;
+
+import org.apache.mina.core.session.IoSession;
+
+public class ClientInfo {
+	private long heartbeatTime = 0;// 心跳时间
+	private int heartbeatCount = 0;// 心跳次数
+	private long protocolTime = 0;// 协议时间
+	private int protocolCount = 0;// 协议次数
+	private long moveTime = 0;// 移动时间
+	private int moveCount = 0;// 移动次数
+	private IoSession ioSession;
+	private Player player;
+	private Timer timer = new Timer();
+
+	public ClientInfo(IoSession ioSession) {
+		this.ioSession = ioSession;
+	}
+
+	public long getHeartbeatTime() {
+		return heartbeatTime;
+	}
+
+	public void setHeartbeatTime(long heartbeatTime) {
+		this.heartbeatTime = heartbeatTime;
+	}
+
+	public int getHeartbeatCount() {
+		return heartbeatCount;
+	}
+
+	public void setHeartbeatCount(int heartbeatCount) {
+		this.heartbeatCount = heartbeatCount;
+	}
+
+	public long getProtocolTime() {
+		return protocolTime;
+	}
+
+	public void setProtocolTime(long protocolTime) {
+		this.protocolTime = protocolTime;
+	}
+
+	public int getProtocolCount() {
+		return protocolCount;
+	}
+
+	public void setProtocolCount(int protocolCount) {
+		this.protocolCount = protocolCount;
+	}
+
+	public IoSession getIoSession() {
+		return ioSession;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public long getMoveTime() {
+		return moveTime;
+	}
+
+	public void setMoveTime(long moveTime) {
+		this.moveTime = moveTime;
+	}
+
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+	public void setMoveCount(int moveCount) {
+		this.moveCount = moveCount;
+	}
+
+	public void addHeartbeatCount() {
+		this.heartbeatCount++;
+	}
+	public void addProtocolCount() {
+		this.protocolCount++;
+	}
+	public void addMovecount() {
+		this.moveCount++;
+	}
+	public Timer getTimer() {
+		return timer;
+	}
+
+}
---
+++ APP-DispatchServer/src/com/app/dispatch/vo/Room.java
@@ -0,0 +1,25 @@
+package com.app.dispatch.vo;
+
+/***
+ * 房间
+ * 
+ * @author doter
+ */
+
+public class Room {
+	private ClientInfo client_1;
+	private ClientInfo client_2;
+	public ClientInfo getClient_1() {
+		return client_1;
+	}
+	public void setClient_1(ClientInfo client_1) {
+		this.client_1 = client_1;
+	}
+	public ClientInfo getClient_2() {
+		return client_2;
+	}
+	public void setClient_2(ClientInfo client_2) {
+		this.client_2 = client_2;
+	}
+
+}
